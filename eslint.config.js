619fb9ab2e54d5e094eeb003f98fed4c16936a24
fei <117238821+feimb@users.noreply.github.com>
2026-08-16 02:04:50 -0300

    añado la estructura general de proyecto, y instalacion de tailwind

--- eslint.config.js
+++ eslint.config.js
@@ -6,6 +6,7 @@ import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
+
   globalIgnores(['dist']),
   {
     files: ['**/*.{ts,tsx}'],
