201fcbba4a8092f02ba6714c324bfc78d91ffc2a
fei <117238821+feimb@users.noreply.github.com>
2026-08-21 02:20:50 -0300

    añado el skeleton loader

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import { MainLayout } from "./layout/MainLayout";
 import { Index } from "./pages/index/Index";
 import { Routes, Route } from "react-router";
+import { SkeletonTheme } from "react-loading-skeleton";
 
 function App() {
     return (
-        <Routes >
-            <Route element={<MainLayout/>} >
-                <Route path="/" element={<Index />} />
-
-            </Route>
-        </Routes>
+        <SkeletonTheme baseColor="#6c727f" highlightColor="#444">
+            <Routes>
+                <Route element={<MainLayout />}>
+                    <Route path="/" element={<Index />} />
+                </Route>
+            </Routes>
+        </SkeletonTheme>
     );
 }
 
--- src/pages/index/Index.tsx
+++ src/pages/index/Index.tsx
@@ -1,9 +1,10 @@
 import { useEffect, useState } from "react";
 import { Card } from "../../layout/Card";
 import getCountry from "../../services/getCountry";
+import Skeleton from "react-loading-skeleton";
 
 export const Index = () => {
-    const [country, setCountry] = useState<any>([]);
+    const [country, setCountry] = useState<any>(null);
     useEffect(() => {
         const fetchCountries = async () => {
             const data = await getCountry();
@@ -11,11 +12,20 @@ export const Index = () => {
             console.log(data);
             setCountry(data);
         };
-        fetchCountries()
+        fetchCountries();
     }, []);
     return (
-        <Card className="min-h-[60vh] py-6 px-8 -mt-4">
-            <p className="text-sm font-bold">Found {country.data.meta.total} countris</p>
+        <Card className="min-h-[60vh] py-4 md:px-8 px-3 -mt-4">
+            {country ? (
+                
+                <p className="text-sm font-bold">
+                    Found {country.data.meta.total} countris
+                </p>
+            ):
+                <p className="text-sm font-bold">
+                    <Skeleton count={1} width={140}/>
+                </p>
+            }
         </Card>
     );
 };
