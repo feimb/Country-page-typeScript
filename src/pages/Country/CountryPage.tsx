bacb5eb56d32e9bea39707927dab444145bd3bd3
fei <117238821+feimb@users.noreply.github.com>
2026-09-07 16:32:03 -0300

    añado la imagen de una bandera y resuelvo problemas de tipos para el deploy

--- src/pages/Country/CountryPage.tsx
+++ src/pages/Country/CountryPage.tsx
@@ -3,20 +3,25 @@ import { useParams } from "react-router";
 import getCountry from "../../services/getCountry";
 
 export const CountryPage = () => {
-    const { code } = useParams<string>();
+    const { code } = useParams<{ code: string | undefined }>();
     const [country, setCountry] = useState<any>(null);
-    useEffect(()=>{
-        const fetchCountry = async (code:string) =>{
+    useEffect(() => {
+        if (!code) return;
+        const fetchCountry = async () => {
             const data = await getCountry(code);
 
             console.log(data);
             setCountry(data);
-        }
-        fetchCountry(code);
-    },[])
+        };
+        fetchCountry();
+    }, []);
     return (
         <>
-           <div></div>
+            <div>
+                {country && (
+                    <img src={country.data.objects[0].flag.url_png} alt="" />
+                )}
+            </div>
         </>
     );
 };
