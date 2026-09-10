6e71360422a736dab775c2bb3ad6a10c47419e65
fei <117238821+feimb@users.noreply.github.com>
2026-09-10 01:16:30 -0300

    aseguro la carga de la metada de la country

--- src/pages/Country/CountryPage.tsx
+++ src/pages/Country/CountryPage.tsx
@@ -5,23 +5,30 @@ import getCountry from "../../services/getCountry";
 export const CountryPage = () => {
     const { code } = useParams<{ code: string | undefined }>();
     const [country, setCountry] = useState<any>(null);
+
+    const [arrMetaData, setArrMetaData] = useState<
+        Array<{ nameData: string; cant: number }>
+    >([]);
+
     useEffect(() => {
         if (!code) return;
         const fetchCountry = async () => {
             const data = await getCountry(code);
 
+            const infoCountry = await data.data.objects[0]
             console.log(data.data.objects[0]);
-            setCountry(data.data.objects[0]);
+            setArrMetaData([
+                {
+                    nameData: "Population",
+                    cant: infoCountry.population,
+                },
+                { nameData: "Area (km²)", cant: infoCountry.area.kilometers },
+            ]);
+            setCountry(infoCountry);
+
         };
         fetchCountry();
     }, []);
-    const arrMetadata: Array<{ nameData: string; cant: number }> = [
-        {
-            nameData: "Population",
-            cant: country.population,
-        },
-        { nameData: "Area (km²)", cant: country.area.kilometers },
-    ];
     return (
         <>
             <div className="w-65 flex m-auto">
@@ -41,12 +48,21 @@ export const CountryPage = () => {
                             </h4>
                         </div>
                         <div className="">
-                            {arrMetadata.map(({nameData, cant}:{nameData:string; cant:number})=>(
-                                <div>
-                                    <p>{nameData}</p>
-                                    <p>{cant}</p>
-                                </div>
-                            ))} 
+                            {arrMetaData.length != 0 &&
+                                arrMetaData.map(
+                                    ({
+                                        nameData,
+                                        cant,
+                                    }: {
+                                        nameData: string;
+                                        cant: number;
+                                    }) => (
+                                        <div>
+                                            <p>{nameData}</p>
+                                            <p>{cant}</p>
+                                        </div>
+                                    ),
+                                )}
                         </div>
                     </div>
                 )}
