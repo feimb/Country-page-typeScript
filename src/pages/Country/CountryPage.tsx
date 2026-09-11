30e29556b335ed7cd003d78128013c97cab2085c
fei <117238821+feimb@users.noreply.github.com>
2026-09-11 01:12:31 -0300

    añado los estilos a la metadata

--- src/pages/Country/CountryPage.tsx
+++ src/pages/Country/CountryPage.tsx
@@ -15,7 +15,7 @@ export const CountryPage = () => {
         const fetchCountry = async () => {
             const data = await getCountry(code);
 
-            const infoCountry = await data.data.objects[0]
+            const infoCountry = await data.data.objects[0];
             console.log(data.data.objects[0]);
             setArrMetaData([
                 {
@@ -25,19 +25,18 @@ export const CountryPage = () => {
                 { nameData: "Area (km²)", cant: infoCountry.area.kilometers },
             ]);
             setCountry(infoCountry);
-
         };
         fetchCountry();
     }, []);
     return (
         <>
-            <div className="w-65 flex m-auto">
+            <div className="w-full flex  justify-center ">
                 {country && (
                     <div>
                         <img
                             src={country.flag.url_png}
                             alt=""
-                            className="-mt-16 rounded-lg"
+                            className=" w-65 -mt-16 rounded-lg flex mx-auto"
                         />
                         <div className="text-center mt-6">
                             <h3 className="text-lg font-bold">
@@ -47,7 +46,7 @@ export const CountryPage = () => {
                                 {country.names.official}
                             </h4>
                         </div>
-                        <div className="">
+                        <div className="mt-6 flex gap-8">
                             {arrMetaData.length != 0 &&
                                 arrMetaData.map(
                                     ({
@@ -57,9 +56,9 @@ export const CountryPage = () => {
                                         nameData: string;
                                         cant: number;
                                     }) => (
-                                        <div key={nameData}>
-                                            <p>{nameData}</p>
-                                            <p>{cant}</p>
+                                        <div key={nameData} className="p-3 bg-border-primary rounded-lg flex items-center gap-2">
+                                            <p className="pr-2 text-sm border-r border-primary">{nameData}</p>
+                                            <p className="">{cant}</p>
                                         </div>
                                     ),
                                 )}
