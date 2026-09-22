72f8c3256d33516bbb2d6c8ffe8cb34dd8a1760b
fei <117238821+feimb@users.noreply.github.com>
2026-09-21 23:43:54 -0300

    añado borde abajo de las cards

--- src/layout/Card.tsx
+++ src/layout/Card.tsx
@@ -2,7 +2,7 @@ import type React from "react";
 
 export const Card = ({children, className}:{children:React.ReactNode, className?:string}) => {
   return (
-    <div className={` w-[95%]  bg-primary mx-auto border-2 border-border-primary rounded-lg ${className}`}>
+    <div className={` w-[95%]  bg-primary mx-auto border-2 border-border-primary rounded-lg ${className} mb-12`}>
         {children}
     </div>
   )
